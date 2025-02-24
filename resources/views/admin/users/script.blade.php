e57e43cbaacb1c7b429f112eca04377730e63d75
MacBook Air <firchun025@gmail.com>
2025-02-24 09:25:14 +0900

    update revisi

--- resources/views/admin/users/script.blade.php
+++ resources/views/admin/users/script.blade.php
@@ -6,6 +6,9 @@
                 serverSide: false,
                 responsive: false,
                 ajax: '{{ url('users-datatable', $users) }}',
+                order: [
+                    [0, 'desc']
+                ],
                 columns: [{
                         data: 'id',
                         name: 'id'
