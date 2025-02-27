22fd6a9deb099565db0de48e61d56990597b46df
MacBook Air <firchun025@gmail.com>
2025-02-27 18:41:27 +0900

    update tracking line

--- resources/views/admin/tracking/index.blade.php
+++ resources/views/admin/tracking/index.blade.php
@@ -61,6 +61,9 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    {{-- routeing assset --}}
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
+    <script src="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.js"></script>
 
     <script>
         document.addEventListener("DOMContentLoaded", function() {
